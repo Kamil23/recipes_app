442d5f2e494641b802998840fecfadcfe2106777
Kamil Cieplicki <kamil@cieplicki.dev>
2023-11-24 15:31:35 +0100

    chore: remove unnecessary lines of code

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import AppNavigation from './src/navigation/AppNavigation';
 
 export default function App() {
@@ -7,12 +5,3 @@ export default function App() {
     <AppNavigation />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
